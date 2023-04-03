5beee6a546f82cfafd5a8cb29d0cd4c84ece70d4
Jed Fox <git@jedfox.com>
2023-04-02 22:43:56 -0400

    Refactor: extract code to set X-Tailscale headers from whois

--- src/serve/proxy.go
+++ src/serve/proxy.go
@@ -8,6 +8,8 @@ import (
 	"tailproxy/src/config"
 	"tailproxy/src/ts"
 	"time"
+
+	"tailscale.com/client/tailscale/apitype"
 )
 
 func makeProxy() http.Handler {
@@ -31,34 +33,38 @@ func makeProxy() http.Handler {
 				r.Out.Header.Set("X-Tailscale-WhoIs", "error")
 			} else {
 				r.Out.Header.Set("X-Tailscale-WhoIs", "ok")
-				r.Out.Header.Set("X-Tailscale-User", who.UserProfile.ID.String())
-				r.Out.Header.Set("X-Tailscale-User-LoginName", who.UserProfile.LoginName)
-				r.Out.Header.Set("X-Tailscale-User-DisplayName", who.UserProfile.DisplayName)
-				if who.UserProfile.ProfilePicURL != "" {
-					r.Out.Header.Set("X-Tailscale-User-ProfilePicURL", who.UserProfile.ProfilePicURL)
-				}
-
-				if len(who.Caps) > 0 {
-					r.Out.Header.Set("X-Tailscale-Caps", strings.Join(who.Caps, ", "))
-				}
-
-				r.Out.Header.Set("X-Tailscale-Node", who.Node.ID.String())
-				r.Out.Header.Set("X-Tailscale-Node-Name", who.Node.ComputedName)
-				if len(who.Node.Capabilities) > 0 {
-					r.Out.Header.Set("X-Tailscale-Node-Caps", strings.Join(who.Node.Capabilities, ", "))
-				}
-				if len(who.Node.Tags) > 0 {
-					r.Out.Header.Set("X-Tailscale-Node-Tags", strings.Join(who.Node.Tags, ", "))
-				}
-				data, err := who.Node.Hostinfo.MarshalJSON()
-				if err == nil {
-					r.Out.Header.Set("X-Tailscale-Hostinfo", string(data))
-				}
+				setWhoIsHeaders(*who, r.Out.Header)
 			}
 		},
 		ModifyResponse: func(r *http.Response) error {
 			fmt.Printf("tailproxy: %v %v %v %v %v\n", r.Request.RemoteAddr, r.Request.Method, r.Request.URL, r.StatusCode, time.Since(start))
 			return nil
 		},
+	}
+}
+
+func setWhoIsHeaders(who apitype.WhoIsResponse, h http.Header) {
+	h.Set("X-Tailscale-User", who.UserProfile.ID.String())
+	h.Set("X-Tailscale-User-LoginName", who.UserProfile.LoginName)
+	h.Set("X-Tailscale-User-DisplayName", who.UserProfile.DisplayName)
+	if who.UserProfile.ProfilePicURL != "" {
+		h.Set("X-Tailscale-User-ProfilePicURL", who.UserProfile.ProfilePicURL)
+	}
+
+	if len(who.Caps) > 0 {
+		h.Set("X-Tailscale-Caps", strings.Join(who.Caps, ", "))
+	}
+
+	h.Set("X-Tailscale-Node", who.Node.ID.String())
+	h.Set("X-Tailscale-Node-Name", who.Node.ComputedName)
+	if len(who.Node.Capabilities) > 0 {
+		h.Set("X-Tailscale-Node-Caps", strings.Join(who.Node.Capabilities, ", "))
+	}
+	if len(who.Node.Tags) > 0 {
+		h.Set("X-Tailscale-Node-Tags", strings.Join(who.Node.Tags, ", "))
+	}
+	data, err := who.Node.Hostinfo.MarshalJSON()
+	if err == nil {
+		h.Set("X-Tailscale-Hostinfo", string(data))
 	}
 }
