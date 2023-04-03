2cbf0f1ea879030697fa122e36afec575b6a35f0
Jed Fox <git@jedfox.com>
2023-04-03 08:20:43 -0400

    Timing info is useless for GetCertificate

--- src/serve/https.go
+++ src/serve/https.go
@@ -5,17 +5,12 @@ import (
 	"net/http"
 	"tailproxy/src/logger"
 	"tailproxy/src/ts"
-	"time"
 )
 
 func ServeHTTPS() {
 	tcpListener := ts.ListenTailnet(443)
 	httpsListener := tls.NewListener(tcpListener, &tls.Config{
-		GetCertificate: func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
-			start := time.Now()
-			defer logger.Log("GetCertificate took %v", time.Since(start))
-			return ts.GetCertificate(hello)
-		},
+		GetCertificate: ts.GetCertificate,
 	})
 	defer httpsListener.Close()
 	if err := http.Serve(httpsListener, makeProxy(false)); err != nil {
